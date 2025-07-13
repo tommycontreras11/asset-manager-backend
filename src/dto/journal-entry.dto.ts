b8cf7c3a24d6bc4ad7b19665e43504b2b12ec3fc
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-07-12 22:45:42 -0400

    fix: update journal entry dto

--- src/dto/journal-entry.dto.ts
+++ src/dto/journal-entry.dto.ts
@@ -1,5 +1,17 @@
-import { IsEnum, IsNotEmpty, IsNumberString, IsOptional, IsString, IsUUID, Matches } from "class-validator";
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumberString,
+  IsOptional,
+  IsString,
+  IsUUID,
+  Matches,
+} from "class-validator";
 import { StatusEnum, StatusType } from "../constants";
+import {
+  MovementType,
+  MovementTypeEnum,
+} from "./../database/entities/entity/journal-entry.entity";
 
 export class CreateJournalEntryDTO {
   @IsNotEmpty()
@@ -13,18 +25,21 @@ export class CreateJournalEntryDTO {
   @IsNotEmpty()
   @IsNumberString()
   @Matches(/^\d{1,8}(\.\d{1,2})?$/, {
-    message:
-      "Amount must have at most 10 digits in total and 2 decimal places",
+    message: "Amount must have at most 10 digits in total and 2 decimal places",
   })
   amount: string;
 
   @IsNotEmpty()
-  @IsUUID('4')
+  @IsUUID("4")
   inventoryTypeUUID: string;
 
   @IsNotEmpty()
-  @IsUUID('4')
+  @IsUUID("4")
   ledgerAccountUUID: string;
+
+  @IsNotEmpty()
+  @IsEnum(MovementTypeEnum)
+  movementType: MovementType;
 }
 
 export class UpdateJournalEntryDTO {
@@ -39,18 +54,21 @@ export class UpdateJournalEntryDTO {
   @IsOptional()
   @IsNumberString()
   @Matches(/^\d{1,8}(\.\d{1,2})?$/, {
-    message:
-      "Amount must have at most 10 digits in total and 2 decimal places",
+    message: "Amount must have at most 10 digits in total and 2 decimal places",
   })
   amount: string;
 
   @IsOptional()
-  @IsUUID('4')
+  @IsUUID("4")
   inventoryTypeUUID: string;
 
   @IsOptional()
-  @IsUUID('4')
+  @IsUUID("4")
   ledgerAccountUUID: string;
+
+  @IsOptional()
+  @IsEnum(MovementTypeEnum)
+  movementType: MovementType;
 
   @IsOptional()
   @IsEnum(StatusEnum)
