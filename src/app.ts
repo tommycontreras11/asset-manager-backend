e0603b14f495118b09d6de06389003a038c91db0
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-08-02 20:03:56 -0400

    refactor: remove custom CORS configuration for default settings

--- src/app.ts
+++ src/app.ts
@@ -7,22 +7,7 @@ import routers from './routers'
 const app = express()
 
 app.use(morgan('dev'))
-const allowedOrigins = [
-  'http://localhost:3001',
-  'https://asset-manager-frontend-e9gg.onrender.com'
-];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
-
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 app.use(routers)
